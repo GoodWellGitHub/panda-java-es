e8b357bbe56058c29c517b4b4c962a7c0200d4df
hjwang <2452255928@qq.com>
2019-06-10 16:24:23 +0800

    search

--- src/main/java/com/org/java/es/demo/JavaElasticsearchToolDemo.java
+++ src/main/java/com/org/java/es/demo/JavaElasticsearchToolDemo.java
@@ -2,25 +2,33 @@ package com.org.java.es.demo;
 
 import com.org.java.es.util.ElasticsearchTool;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class JavaElasticsearchToolDemo {
     public static void main(String[] args) {
         ElasticsearchTool tool = new ElasticsearchTool("red_floor", "1");
         ElasticsearchTool.init();
-       // tool.synchronousExecution("red_floor", "1");
+        // tool.synchronousExecution("red_floor", "1");
         //tool.asynchronousExecution();
 
+        /*
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("name", "林黛玉");
         map.put("age", 18);
+        */
+
         //map.put("sex", "女");
 
         //tool.buildIndex("user_floor", "1", map);
         //tool.buildIndex("red_floor", null, map);
 
-        tool.termQuery("red_floor","name","林黛玉");
+        // tool.termQuery("red_floor","name","林黛玉");
+
+        // tool.bulkApi();
+
+        //tool.multiGet();
+
+        //tool.multiTerm();
+
+        tool.search();
 
 
         ElasticsearchTool.close();
--- src/main/java/com/org/java/es/util/ElasticsearchTool.java
+++ src/main/java/com/org/java/es/util/ElasticsearchTool.java
@@ -3,11 +3,15 @@ package com.org.java.es.util;
 import io.netty.util.internal.StringUtil;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.*;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.ActiveShardCount;
 import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.action.update.UpdateRequest;
@@ -15,17 +19,31 @@ import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.core.MultiTermVectorsRequest;
+import org.elasticsearch.client.core.MultiTermVectorsResponse;
 import org.elasticsearch.client.core.TermVectorsRequest;
 import org.elasticsearch.client.core.TermVectorsResponse;
 import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.VersionType;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.script.ScriptType;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.ScoreSortBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ElasticsearchTool {
@@ -58,7 +76,7 @@ public class ElasticsearchTool {
             indexRequest.id(documentId);
         }
         indexRequest.source(content);
-        IndexResponse indexResponse= null;
+        IndexResponse indexResponse = null;
         try {
             indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
         } catch (IOException e) {
@@ -93,9 +111,9 @@ public class ElasticsearchTool {
      * 如果服务器返回4xx或5xx错误代码，则高级客户端会尝试解析响应正文错误详细信息，
      * 然后抛出通用ElasticsearchException并将原始ResponseException作为抑制异常添加到其中。
      */
-    public void synchronousExecution(String index,String documentId) {
+    public void synchronousExecution(String index, String documentId) {
         try {
-            GetRequest request=new GetRequest(index,documentId);
+            GetRequest request = new GetRequest(index, documentId);
             GetResponse response = client.get(request, RequestOptions.DEFAULT);
             System.out.println(response);
         } catch (IOException e) {
@@ -182,8 +200,8 @@ public class ElasticsearchTool {
         System.out.println(response);
     }
 
-    public void termQuery(String index,String filed,String fieldValue) {
-        TermVectorsRequest request=new TermVectorsRequest(index,fieldValue);
+    public void termQuery(String index, String filed, String fieldValue) {
+        TermVectorsRequest request = new TermVectorsRequest(index, fieldValue);
         request.setFields(filed);
 
         request.setFieldStatistics(false);
@@ -192,7 +210,7 @@ public class ElasticsearchTool {
         request.setOffsets(false);
         request.setPayloads(false);
 
-        Map<String, Integer> filterSettings = new HashMap<String,Integer>();
+        Map<String, Integer> filterSettings = new HashMap<String, Integer>();
         filterSettings.put("max_num_terms", 3);
         filterSettings.put("min_term_freq", 1);
         filterSettings.put("max_term_freq", 10);
@@ -202,14 +220,14 @@ public class ElasticsearchTool {
         filterSettings.put("max_word_length", 10);
 
         request.setFilterSettings(filterSettings);
-        Map<String, String> perFieldAnalyzer = new HashMap<String,String>();
+        Map<String, String> perFieldAnalyzer = new HashMap<String, String>();
         perFieldAnalyzer.put("user", "keyword");
         request.setPerFieldAnalyzer(perFieldAnalyzer);
 
         request.setRealtime(false);
         request.setRouting("routing");
 
-        TermVectorsResponse response= null;
+        TermVectorsResponse response = null;
         try {
             response = client.termvectors(request, RequestOptions.DEFAULT);
         } catch (IOException e) {
@@ -227,6 +245,176 @@ public class ElasticsearchTool {
         boolean found = response.getFound();
         System.out.println(found);
 
+    }
+
+    /**
+     * source中有的数据更改，source中没有的字段，会被删除
+     */
+    public void bulkApi() {
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.add(new IndexRequest("blogs").id("7PRkJ2sB5FGi1ceMqOMw").source(XContentType.JSON, "title", "虚拟机安装"));
+        bulkRequest.add(new IndexRequest("blogs").id("mget").source(XContentType.JSON, "title", "无效数据"));
+        try {
+            BulkResponse bulkItemResponses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+            for (BulkItemResponse bulkItemResponse : bulkItemResponses) {
+                if (bulkItemResponse.isFailed()) {
+                    BulkItemResponse.Failure failure = bulkItemResponse.getFailure();
+                    System.out.println(failure);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void multiGet() {
+        MultiGetRequest multiGetRequest = new MultiGetRequest();
+        multiGetRequest.add(new MultiGetRequest.Item("blogs", "7PRkJ2sB5FGi1ceMqOMw"));
+        // multiGetRequest.add(new MultiGetRequest.Item("red_floor","1"));
+        String[] includes = new String[]{"18"};
+        String[] excludes = Strings.EMPTY_ARRAY;
+        FetchSourceContext context = new FetchSourceContext(true, includes, excludes);
+        multiGetRequest.add(new MultiGetRequest.Item("red_floor", "1").fetchSourceContext(context));
+        try {
+            MultiGetResponse response = client.mget(multiGetRequest, RequestOptions.DEFAULT);
+            MultiGetItemResponse[] responses = response.getResponses();
+            for (MultiGetItemResponse firstItem : responses) {
+                //assertNull(firstItem.getFailure());
+                GetResponse firstGet = firstItem.getResponse();
+                String index = firstItem.getIndex();
+                System.out.println("index: " + index);
+                String id = firstItem.getId();
+                System.out.println("id : " + id);
+                if (firstGet.isExists()) {
+                    long version = firstGet.getVersion();
+                    System.out.println("version: " + version);
+                    String sourceAsString = firstGet.getSourceAsString();
+                    System.out.println("sourceAsString: " + sourceAsString);
+                    Map<String, Object> sourceAsMap = firstGet.getSourceAsMap();
+                    System.out.println("sourceAsMap : " + sourceAsMap);
+                    byte[] sourceAsBytes = firstGet.getSourceAsBytes();
+                } else {
+                    System.out.println("无数据");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void multiTerm() {
+        MultiTermVectorsRequest multiTermVectorsRequest = new MultiTermVectorsRequest();
+        TermVectorsRequest termVectorsRequest = new TermVectorsRequest("red_floor", "1");
+        termVectorsRequest.setFields("age");
+        multiTermVectorsRequest.add(termVectorsRequest);
+
+
+        TermVectorsRequest tvrequestTemplate =
+                new TermVectorsRequest("red_floor", "fake_id");
+        tvrequestTemplate.setFields("user");
+        String[] ids = {"1", "2"};
+        MultiTermVectorsRequest request = new MultiTermVectorsRequest(ids, tvrequestTemplate);
+        try {
+            MultiTermVectorsResponse response = client.mtermvectors(multiTermVectorsRequest, RequestOptions.DEFAULT);
+            List<TermVectorsResponse> tvresponseList = response.getTermVectorsResponses();
+            if (tvresponseList != null) {
+                for (TermVectorsResponse tvresponse : tvresponseList) {
+                    System.out.println(tvresponse.getIndex() + tvresponse.getId());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void search() {
+     /*
+      SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+        try {
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHits searchHits = searchResponse.getHits();
+            for (SearchHit searchHit : searchHits.getHits()) {
+                System.out.println("##################");
+                System.out.println(searchHit.getIndex());
+                System.out.println(searchHit.getSourceAsMap());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+      /*
+       SearchRequest searchRequest = new SearchRequest("red_floor");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+        try {
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHits searchHits = searchResponse.getHits();
+            for (SearchHit searchHit : searchHits.getHits()) {
+                System.out.println("##################");
+                System.out.println(searchHit.getIndex());
+                System.out.println(searchHit.getSourceAsMap());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+/*        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.termQuery("sex", "女"));
+        searchSourceBuilder.from(0);
+        searchSourceBuilder.size(5);
+        searchRequest.source(searchSourceBuilder);
+        try {
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHits searchHits = searchResponse.getHits();
+            for (SearchHit searchHit : searchHits.getHits()) {
+                System.out.println("##################");
+                System.out.println(searchHit.getIndex());
+                System.out.println(searchHit.getSourceAsMap());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+        SearchRequest searchRequest = new SearchRequest("red_floor");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(new MatchQueryBuilder("age","1").fuzziness(Fuzziness.AUTO)
+                .prefixLength(3)
+                .maxExpansions(10));
+        searchRequest.source(searchSourceBuilder);
+        searchSourceBuilder.sort(new ScoreSortBuilder().order(SortOrder.DESC));
+        searchSourceBuilder.sort(new FieldSortBuilder("_id").order(SortOrder.ASC));
+
+        String[] includeFields = new String[] {"title", "innerObject.*"};
+        String[] excludeFields = new String[] {"user"};
+        searchSourceBuilder.fetchSource(includeFields, excludeFields);
+
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        HighlightBuilder.Field highlightTitle =
+                new HighlightBuilder.Field("title");
+        highlightTitle.highlighterType("unified");
+        highlightBuilder.field(highlightTitle);
+        HighlightBuilder.Field highlightUser = new HighlightBuilder.Field("user");
+        highlightBuilder.field(highlightUser);
+        searchSourceBuilder.highlighter(highlightBuilder);
+
+        try {
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            SearchHits searchHits = searchResponse.getHits();
+            for (SearchHit searchHit : searchHits.getHits()) {
+                System.out.println("##################");
+                System.out.println(searchHit.getIndex());
+                System.out.println(searchHit.getSourceAsMap());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
